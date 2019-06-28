1147461cd44d4438fc8f1e01ee9781ccc3c850d7
Philip Vieira <philipvieira00@gmail.com>
2019-06-27 21:13:12 -0400

    Added wireframe of future methods.

--- api.js
+++ api.js
@@ -1,2 +1,30 @@
+//Spotify API Key
 let spotifyApiKey = "0c13fbb948dc49b19bd7e6675f6ae981";
+//YouTube API Key
 let youtubeAPIKey = "AIzaSyDuAVXHcx-uIsVKglPzuOODT5RVMnnqztU";
+
+//Used to determine if an Artist or Genre is in fact real. If they aren't, it will let the user know that the genre or artist doesn't exist.
+function isReal() { 
+
+}
+
+//Sends a call to Spotify in order to get the top rated songs from an artist under that genre. 
+function getGenre() { 
+
+}
+
+//Used to get the artist information from Spotify including their albums and top rated songs.
+function getArtist() { 
+
+}
+
+//Used to acquire the songs from Spotify
+function getSong() { 
+
+}
+
+//Returns the video from YouTube for that specific song by that specific artist.
+function getVideo() {
+
+}
+
